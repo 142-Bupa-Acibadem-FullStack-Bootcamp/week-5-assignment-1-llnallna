763bd046d3a5200c12ca7ed30b62bb29d0e0c79f
Anıl Çetin <75864563+llnallna@users.noreply.github.com>
2022-01-25 00:48:38 +0300

    5.Hafta 1.Odev tamamlandi

---
+++ NorthwindBupa.Interface/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using NorthwindBupa.Entity.Dto;
+using NorthwindBupa.Entity.IBase;
+using NorthwindBupa.Entity.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthwindBupa.Interface
+{
+    public interface IUserService : IGenericService<User, DtoUser>
+    {
+        IResponse<DtoUserToken> Login(DtoLogin login);
+    }
+}
---
+++ NorthwindBupa.WebApi/Controllers/UserController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NorthwindBupa.Entity.Base;
+using NorthwindBupa.Entity.Dto;
+using NorthwindBupa.Entity.IBase;
+using NorthwindBupa.Entity.Models;
+using NorthwindBupa.Interface;
+using NorthwindBupa.WebApi.Base;
+using System;
+
+namespace NorthwindBupa.WebApi.Controllers
+{ 
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService userService;
+        public UserController(IUserService userService) 
+        {
+            this.userService = userService;
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        public IResponse<DtoUserToken> Login(DtoLogin login)
+        {
+            try
+            {
+                return userService.Login(login);
+            }
+            catch (Exception ex)
+            {
+                return new Response<DtoUserToken>
+                {
+                    Message = $"Error:{ex.Message}",
+                    StatusCode = StatusCodes.Status500InternalServerError,
+                    Data = null
+                };
+            }
+        }
+    }
+}
